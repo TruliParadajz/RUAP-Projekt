20d8ada9c80fa9a3755896c2394bd9efaf98b9da
TruliParadajz <nargz44@gmail.com>
2018-03-27 13:06:03 +0200

    Pokusaj1
    nflikti zakomentirani ;)

--- LV1_RUAP/LV1_RUAP/LV1_RUAP.cpp
+++ LV1_RUAP/LV1_RUAP/LV1_RUAP.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
+//<<<<<<< HEAD
 	printf("Pozdrav iz aplikacije od Filipa Begica, ne znam broj indeksa");
+//=======
+	printf("Pozdrav iz aplikacije od Filipa Begica");
+//>>>>>>> 82fb6d2... Pokusaj1
 	getch();
 }
